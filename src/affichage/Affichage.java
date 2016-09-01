509eb0e9fbfffd9cfcc4112a9f06bae961221ee4
Laura <laurajumelle@hotmail.fr>
2016-09-01 09:38:36 +0200

    Ajout de l'affichage

--- src/affichage/Affichage.java
+++ src/affichage/Affichage.java
@@ -2,4 +2,24 @@ package affichage;
 
 public class Affichage {
 
+	public static void main(String args[]) {
+		for (int j = 0; j < 11; j++) {
+			if (j == 0 || j == 10)
+				for (int i = 0; i < 11; i++) {
+					System.out.print(" * ");
+				}
+			else
+				for (int i =0 ; i<11;i++) {
+					if(i==0 || i==10)
+						System.out.print(" * ");
+					else
+						System.out.print("   ");
+				}
+			System.out.println();
+		}
+		
+	}
+	
+	
+
 }
