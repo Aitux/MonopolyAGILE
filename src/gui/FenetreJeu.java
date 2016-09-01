0a80c27019d6aa64eb3a24bdec118c1c7550597d
Hexafile <thomas.plouchart@hotmail.fr>
2016-09-01 17:30:24 +0200

    Gui edited

--- src/gui/FenetreJeu.java
+++ src/gui/FenetreJeu.java
@@ -1,18 +1,43 @@
 package gui;
 
-import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class FenetreJeu extends JFrame{
+	
+	BufferedImage buffer;
+	Image image;
+	
 	public FenetreJeu(){
 		
+		/*try {
+			buffer = ImageIO.read(this.getClass().getResource("monopoly-classique-plateau.jpg"));
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}*/
+		image = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("monopoly-classique-plateau.jpg"));
 		
+		Component imageComp = new JComponent() {
+			@Override
+			public void paint(Graphics arg0) {
+				super.paint(arg0);
+				arg0.drawImage(image, 0, 0, this);
+			}
+		};
 		
-		setSize(new Dimension(500, 500));
+		add(imageComp);
+		
+		setSize(new Dimension(1241, 1241));
 		setMinimumSize(getSize());
 		setTitle("Monopoly");
 		
