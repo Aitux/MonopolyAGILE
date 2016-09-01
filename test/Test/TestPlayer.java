12bbc1572e31db4b6d7de550089028bc4d459cad
Hexafile <thomas.plouchart@hotmail.fr>
2016-09-01 14:20:26 +0200

    Refresh

--- test/Test/TestPlayer.java
+++ test/Test/TestPlayer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Test;
 
 import static org.junit.Assert.*;
@@ -14,24 +13,4 @@ public class TestPlayer {
 		Player p= new Player();
 		assertTrue(p.move()<13 && p.move()>1);
 	}
-
-	
-=======
-package Test;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import entite.Player;
-
-public class TestPlayer {
-
-	@Test
-	public void testMove() {
-		Player p= new Player();
-		assertTrue(p.move()<13 && p.move()>1);
-	}
-	
->>>>>>> 3c735e2fe0c28ac47720a7cd050ca81560d33c3e
 }
