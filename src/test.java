90c64f1cb23bae801404076640d03c7537ad1322
tartarev <tartarev@OffensiveBias>
2016-09-01 08:39:19 +0200

    test

--- src/test.java
+++ src/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld2");
 	}
 
 }
