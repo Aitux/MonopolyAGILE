510c640de6bf32eea7571b1d040cc05a109e6a2c
Hexafile <thomas.plouchart@hotmail.fr>
2016-09-01 16:18:50 +0200

    TestDes

---
+++ test/Test/TestDes.java
@@ -0,0 +1,23 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import entite.Des;
+
+public class TestDes {
+	Des d;
+	
+	@Test
+	public void TestDesRandom(){
+		d = new Des();
+		assertTrue(d.getA()>0 && d.getA()<7 && d.getB()>0 && d.getB()<7);
+	}
+	
+	@Test
+	public void TestDoublon(){
+		d = new Des(1,1);
+		assertTrue(d.isDoublon());
+	}
+}
