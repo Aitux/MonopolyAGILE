eb1e83f4a1828fa508d5eee4ac38c98d0f42c7ba
Simon VANDPUTTE <Aitux@users.noreply.github.com>
2016-09-01 09:18:00 +0200

    Back to abstract

--- src/heritage/Case.java
+++ src/heritage/Case.java
@@ -2,10 +2,14 @@ package heritage;
 
 import java.util.ArrayList;
 
-public interface Case {
+import entite.Joueur;
+import enumeration.TypeCase;
+
+public abstract class Case {
 
 	ArrayList<Joueur> JoueurSurCase;
 	String nom; // nom de la case
+	TypeCase Type;
 	
 	
 }
