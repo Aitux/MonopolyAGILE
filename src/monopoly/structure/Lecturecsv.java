49c4d59efd63b58cbfefc9f52e73ae52190215be
Simon VANDPUTTE <Aitux@users.noreply.github.com>
2016-09-02 09:50:01 +0200

    Ajout argent

--- src/monopoly/structure/Lecturecsv.java
+++ src/monopoly/structure/Lecturecsv.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Lecturecsv {
+public class Lecturecsv {	
 
 	
 //	public static void main(String[] args){
